148ca309cb02e2bb2fae7c260123e2038bf71222
MikeJeba <jebajovitha@gmail.com>
2024-04-15 10:28:00 +0530

    Six Commit

---
+++ src/BasicJava/Six.java
@@ -0,0 +1,11 @@
+package BasicJava;
+import java.util.*;
+public class Six {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the rupee: ");
+        double rup = sc.nextInt();
+        double res = rup / 84.0;
+        System.out.println(res);
+    }
+}
