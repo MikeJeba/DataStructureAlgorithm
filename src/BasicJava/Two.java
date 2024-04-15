47add517b4de45bd548960dbe075fc3e68c497d7
MikeJeba <jebajovitha@gmail.com>
2024-04-15 09:40:02 +0530

    Second Commit

---
+++ src/BasicJava/Two.java
@@ -0,0 +1,10 @@
+package BasicJava;
+import java.util.*;
+public class Two {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.next();
+        System.out.print("Hello," + name);
+    }
+}
