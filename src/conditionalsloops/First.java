53f23d2f27ce89c860cedef8973c28900bc54d81
MikeJeba <jebajovitha@gmail.com>
2024-04-23 09:59:50 +0530

    First Commit

--- src/conditionalsloops/First.java
+++ src/conditionalsloops/First.java
@@ -1,4 +1,14 @@
 package conditionalsloops;
 
+import java.util.Scanner;
+
 public class First {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the area: ");
+        double r = sc.nextDouble();
+        double pi = 3.14;
+        double res = pi * Math.pow(r,2);
+        System.out.println(res);
+    }
 }
