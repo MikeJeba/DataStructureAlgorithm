b86678415c85abf67a3f088ff279283857594fcd
MikeJeba <jebajovitha@gmail.com>
2024-04-24 22:05:22 +0530

    Fifth Commit

---
+++ src/conditionalsloops/Fifth.java
@@ -0,0 +1,13 @@
+package conditionalsloops;
+import java.util.*;
+public class Fifth {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the height: ");
+        int h = in.nextInt();
+        System.out.println("Enter the breath: ");
+        int b = in.nextInt();
+        int area = h * b;
+        System.out.println(area);
+    }
+}
