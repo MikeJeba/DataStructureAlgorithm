aa312120affa9a22f45043512a6ed1c87982d7db
MikeJeba <jebajovitha@gmail.com>
2024-04-24 17:47:15 +0530

    Third Commit

---
+++ src/conditionalsloops/Third.java
@@ -0,0 +1,13 @@
+package conditionalsloops;
+
+import java.util.Scanner;
+
+public class Third {
+    public static void main(String[] args) {
+        rect(5,5);
+    }
+    static void rect(int w,int l){
+        int res = w * l;
+        System.out.println(res);
+    }
+}
