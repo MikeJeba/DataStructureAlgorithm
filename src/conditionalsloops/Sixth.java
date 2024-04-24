b6a75acd7752db804e027364f37efea72517f1ea
MikeJeba <jebajovitha@gmail.com>
2024-04-24 22:25:37 +0530

    Sixth Commit

---
+++ src/conditionalsloops/Sixth.java
@@ -0,0 +1,11 @@
+package conditionalsloops;
+
+public class Sixth {
+    public static void main(String[] args) {
+        int d1 = 10;
+        int d2 = 10;
+        int res = d1 * d2;
+        int fin = res / 2;
+        System.out.println(fin);
+    }
+}
