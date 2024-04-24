f45d3cb374292eeaac963b3ee8244381444f11ed
MikeJeba <jebajovitha@gmail.com>
2024-04-24 21:54:44 +0530

    Fourth Commit

---
+++ src/conditionalsloops/Fourth.java
@@ -0,0 +1,14 @@
+package conditionalsloops;
+
+public class Fourth {
+    public static void main(String[] args) {
+       System.out.println(iso());
+    }
+    static double iso(){
+        int b = 5;
+        int h = 5;
+        int ans = b * h;
+        double res = ans / 2;
+        return res;
+    }
+}
