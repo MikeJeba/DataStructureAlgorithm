01c762f6277af2243e4031a6255eeb6715039ce9
MikeJeba <jebajovitha@gmail.com>
2024-04-25 23:01:50 +0530

    Twelev Commit

---
+++ src/conditionalsloops/Twelev.java
@@ -0,0 +1,10 @@
+package conditionalsloops;
+
+public class Twelev {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 4 * a;
+        System.out.println(b);
+    }
+}
