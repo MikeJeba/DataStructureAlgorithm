8c37564ab4d7e9812047d713cbd1dc90118e3268
MikeJeba <jebajovitha@gmail.com>
2024-04-25 22:58:37 +0530

    Eleven Commit

---
+++ src/conditionalsloops/Eleven.java
@@ -0,0 +1,10 @@
+package conditionalsloops;
+import java.util.Scanner;
+public class Eleven {
+    public static void main(String[] args) {
+        int l = 10;
+        int b = 10;
+        int p = (l + b) * 2;
+        System.out.println(p);
+    }
+}
