91d2391a5619efbf837735cde794f2781e17c492
MikeJeba <jebajovitha@gmail.com>
2024-04-25 23:08:35 +0530

    Thirteen Commit

---
+++ src/conditionalsloops/Thirteen.java
@@ -0,0 +1,9 @@
+package conditionalsloops;
+
+public class Thirteen {
+    public static void main(String[] args) {
+        int a = 10;
+        int res = 4 * a;
+        System.out.println(res);
+    }
+}
