ac7bce686afdf726ce9a8317cfe378df3ed41c47
MikeJeba <jebajovitha@gmail.com>
2024-04-25 22:40:22 +0530

    Nine Commit

---
+++ src/conditionalsloops/Nine.java
@@ -0,0 +1,11 @@
+package conditionalsloops;
+import java.util.Scanner;
+public class Nine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value: ");
+        double a = sc.nextDouble();
+        double res = 3 * a;
+        System.out.println(res);
+    }
+}
