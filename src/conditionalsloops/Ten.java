29053fd2af4cc50eac0ea9fe60249b54c6fedd83
MikeJeba <jebajovitha@gmail.com>
2024-04-25 22:49:37 +0530

    Ten Commit

---
+++ src/conditionalsloops/Ten.java
@@ -0,0 +1,10 @@
+package conditionalsloops;
+import java.util.Scanner;
+public class Ten {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int c = 2 * (a + b);
+        System.out.println(c);
+    }
+}
