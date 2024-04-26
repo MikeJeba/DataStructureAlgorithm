10e842c5cd36e991ccad6d1222ea2bce6950ced2
MikeJeba <jebajovitha@gmail.com>
2024-04-26 23:29:58 +0530

    Sixteen Commit

---
+++ src/conditionalsloops/Sixteen.java
@@ -0,0 +1,11 @@
+package conditionalsloops;
+import java.util.*;
+public class Sixteen {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        double r = 5;
+        double h = 10;
+        double ans = pi * r * r * h;
+        System.out.println(ans);
+    }
+}
