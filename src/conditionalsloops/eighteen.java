0c2badfa3b71a626d19e936d449014982fdcf64c
MikeJeba <jebajovitha@gmail.com>
2024-04-26 23:37:48 +0530

    eighteen Commit

---
+++ src/conditionalsloops/eighteen.java
@@ -0,0 +1,11 @@
+package conditionalsloops;
+
+public class eighteen {
+    public static void main(String[] args) {
+        double l = 5;
+        double w = 5;
+        double h = 5;
+        double res = (l * w * h) / 3;
+        System.out.println(res);
+    }
+}
