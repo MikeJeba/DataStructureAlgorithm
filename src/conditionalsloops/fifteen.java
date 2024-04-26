eacb3e1eafd99dfe8459758ea3aa2d22d744a1a8
MikeJeba <jebajovitha@gmail.com>
2024-04-26 23:27:28 +0530

    fifteen Commit

---
+++ src/conditionalsloops/fifteen.java
@@ -0,0 +1,10 @@
+package conditionalsloops;
+import java.util.*;
+public class fifteen {
+    public static void main(String[] args) {
+        int B = 15;
+        int h = 10;
+        int ans = B * h;
+        System.out.println(ans);
+    }
+}
