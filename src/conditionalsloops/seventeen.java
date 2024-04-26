7003926ea244a71ba0bd05bcfd3adfd7fd4523c5
MikeJeba <jebajovitha@gmail.com>
2024-04-26 23:34:31 +0530

    seventeen Commit

---
+++ src/conditionalsloops/seventeen.java
@@ -0,0 +1,11 @@
+package conditionalsloops;
+
+public class seventeen {
+    public static void main(String[] args) {
+        double pi = 3.14;
+        int r = 3;
+        int half = 4 / 3;
+        double res = half * (r * r * r) * pi;
+        System.out.println(res);
+    }
+}
