6cbcd9142a2063dd12777ef61e023d9464c21369
MikeJeba <jebajovitha@gmail.com>
2024-04-26 23:24:05 +0530

    fourteen Commit

---
+++ src/conditionalsloops/fouteen.java
@@ -0,0 +1,12 @@
+package conditionalsloops;
+import java.util.Scanner;
+public class fouteen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double pi = 3.14;
+        double r = 3;
+        double h = 5;
+        double ans = pi * (r * r) * h / 3;
+        System.out.println(ans);
+    }
+}
