0c259efb4d8a4bd4927152038a5a8286b5232b00
MikeJeba <jebajovitha@gmail.com>
2024-06-04 22:28:48 +0530

    Twenty three

--- src/conditionalsloops/Sum.java
+++ src/conditionalsloops/Sum.java
@@ -1,15 +1,19 @@
 package conditionalsloops;
 import java.util.Scanner;
 public class Sum {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number: ");
-        int n = sc.nextInt();
-        int sum = 0;
-        while(n > 0){
-            sum = sum + n;
-            n--;
+    public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            int sum = 0;
+            int input;
+
+            System.out.println("Enter integers (enter 0 to stop):");
+            do {
+                input = scanner.nextInt();
+                sum += input;
+            } while (input != 0);
+
+            System.out.println("Sum of entered integers: " + sum);
         }
-        System.out.println(sum);
     }
-}
+
