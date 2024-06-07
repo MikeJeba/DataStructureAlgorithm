23e65ae806b67070f2fa868073584a0c81aea352
MikeJeba <jebajovitha@gmail.com>
2024-06-07 12:19:16 +0530

    Twenty Eight

--- src/Intermediate/Average.java
+++ src/Intermediate/Average.java
@@ -1,14 +1,14 @@
 package Intermediate;
-import java.util.Scanner;
+
 public class Average {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         int[] n = {1,2,3};
         int len = n.length;
         int res = 0;
         int ans = 1;
-        for(int i = 0; i < len; i++){
-            res += n[i];
+        for (int j : n) {
+            res += j;
             ans = res / len;
         }
         System.out.println(ans);
