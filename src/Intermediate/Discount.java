72a24dc30fad564add1cf15a3ef0a0ce9b27e97a
MikeJeba <jebajovitha@gmail.com>
2024-06-07 12:42:39 +0530

    Twenty Nine

---
+++ src/Intermediate/Discount.java
@@ -0,0 +1,11 @@
+package Intermediate;
+
+public class Discount {
+    public static void main(String[] args) {
+        int m = 50;
+        int s = 45;
+        int d = m - s;
+        float dp = ((float) d / m) * 100;
+        System.out.println(dp);
+    }
+}
