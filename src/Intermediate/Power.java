90552fb9775c18ea7f51e69900a9199a01ebd00e
MikeJeba <jebajovitha@gmail.com>
2024-06-09 14:21:09 +0530

    Thrity Two

---
+++ src/Intermediate/Power.java
@@ -0,0 +1,16 @@
+package Intermediate;
+import java.util.Scanner;
+public class Power {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the element: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the power: ");
+        int b = sc.nextInt();
+        double ans = powerFunc(a,b);
+        System.out.println(ans);
+    }
+    static double powerFunc(int a,int b){
+        return Math.pow(a,b);
+    }
+}
