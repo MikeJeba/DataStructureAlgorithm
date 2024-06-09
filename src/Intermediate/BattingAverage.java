9f86e7285b3b31b28fb6c410c5ad075cddfc6e42
MikeJeba <jebajovitha@gmail.com>
2024-06-09 17:47:39 +0530

    Thrity Four

---
+++ src/Intermediate/BattingAverage.java
@@ -0,0 +1,18 @@
+package Intermediate;
+import java.util.Scanner;
+public class BattingAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the hits: ");
+        int hit = sc.nextInt();
+        System.out.println("Enter the at-bat");
+        int at_bat = sc.nextInt();
+        if(at_bat == 0){
+            System.out.println("No ball one is at bat");
+        }
+        else{
+            int res = hit / at_bat;
+            System.out.println(res);
+        }
+    }
+}
