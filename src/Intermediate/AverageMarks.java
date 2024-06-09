ad6ecba176d7d66142573eff55bf39d06e218070
MikeJeba <jebajovitha@gmail.com>
2024-06-09 22:48:14 +0530

    Thrity Eight

---
+++ src/Intermediate/AverageMarks.java
@@ -0,0 +1,21 @@
+package Intermediate;
+import java.util.Scanner;
+public class AverageMarks {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Marks: ");
+        int[] marks = new int[5];
+        int len = marks.length;
+        for(int j = 0; j < len; j++){
+            marks[j] = sc.nextInt();
+        }
+        int sum = 0;
+        int res = 0;
+        for(int i = 0 ; i < len; i++) {
+             sum += marks[i];
+             res = sum / len;
+        }
+        System.out.println(res);
+    }
+}
