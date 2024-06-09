5a0bb384d7412a04ea6647be584979bff198415d
MikeJeba <jebajovitha@gmail.com>
2024-06-09 17:37:31 +0530

    Thrity Three

---
+++ src/Intermediate/DepreciationCost.java
@@ -0,0 +1,16 @@
+package Intermediate;
+
+import java.util.Scanner;
+public class DepreciationCost {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the initial Cost: ");
+        int initialCost = sc.nextInt();
+        System.out.println("Enter the salvage: ");
+        int salvage = sc.nextInt();
+        System.out.println("Enter the Useful life: ");
+        int usefulLife = sc.nextInt();
+        int res = (initialCost - salvage) / usefulLife;
+        System.out.println(res);
+    }
+}
