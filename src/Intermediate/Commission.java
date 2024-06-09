6bfaa2601b1ee2cea4e6cbd58cb3826c164c27d5
MikeJeba <jebajovitha@gmail.com>
2024-06-09 14:16:00 +0530

    Thrity One

---
+++ src/Intermediate/Commission.java
@@ -0,0 +1,18 @@
+package Intermediate;
+import java.util.Scanner;
+public class Commission {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Total Sale: ");
+        double totalSales = sc.nextDouble();
+        System.out.println("Enter the Commission: ");
+        double commissionRate = sc.nextDouble();
+        double ans = CommissionResult(totalSales,commissionRate);
+        System.out.println(ans);
+    }
+    static double CommissionResult(double totalSales, double commissionRate){
+        return (totalSales * commissionRate) / 100;
+    }
+
+
+}
