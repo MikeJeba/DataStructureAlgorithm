49e920e58faf515851be3bb6d26557c220ddd0ee
MikeJeba <jebajovitha@gmail.com>
2024-06-10 22:20:45 +0530

    Fourty five

---
+++ src/Intermediate/LCM.java
@@ -0,0 +1,20 @@
+package Intermediate;
+public class LCM {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 3;
+        int res = lcmresult(a,b);
+        System.out.print(res);
+    }
+    static int lcm(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    static int lcmresult(int a, int b){
+        return Math.abs(a * b) / lcm(a,b);
+    }
+}
