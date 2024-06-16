741a257120036ef08bce6efdb40e8af9c3789933
MikeJeba <jebajovitha@gmail.com>
2024-06-16 15:02:25 +0530

    Fun nine

---
+++ src/Function/Factorial.java
@@ -0,0 +1,18 @@
+package Function;
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+        Factorial(num);
+    }
+
+    static void Factorial(int num) {
+        int mul = 1;
+        for (int i = 1; i <= num; i++) {
+            mul = mul * i;
+        }
+        System.out.println(mul);
+    }
+}
