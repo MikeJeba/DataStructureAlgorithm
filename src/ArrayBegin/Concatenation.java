eb50e1aaedfdf09a2fe8eba8015f99914693cbfb
MikeJeba <jebajovitha@gmail.com>
2024-06-18 23:19:54 +0530

    Fun thirteen

---
+++ src/ArrayBegin/Concatenation.java
@@ -0,0 +1,18 @@
+package ArrayBegin;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        int[] test = {1,2,1};
+        int[] res = getConcatenation(test);
+
+    }
+    public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        for(int i = 0; i < n; i++){
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
