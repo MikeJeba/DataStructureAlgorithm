7f267a0576dc1769824f75e657b8db9e94fb2cce
MikeJeba <jebajovitha@gmail.com>
2024-07-17 16:56:02 +0530

    Array

--- src/ArrayBegin/minCost.java
+++ src/ArrayBegin/minCost.java
@@ -1,7 +1,21 @@
 package ArrayBegin;
 
 class minCost {
-    public int minCostToMoveChips(int[] position) {
-        return 0;
+    public static void main(String[] args) {
+        int[] position = {2,2,2,3,3};
+        int res = minCostToMoveChips(position);
+        System.out.println(res);
+    }
+    public static int minCostToMoveChips(int[] position) {
+        int e = 0;
+        int o = 0;
+        for(int i : position){
+            if(position[i] % 2 == 0){
+                e++;
+            }else{
+                o++;
+            }
+        }
+        return Math.min(e,o);
     }
 }
