d083ed1d622376f8e54ab45b059df6e38a3d02c5
MikeJeba <jebajovitha@gmail.com>
2025-01-09 22:02:48 +0530

    Flipping

--- src/ArrayBegin/Flipping.java
+++ src/ArrayBegin/Flipping.java
@@ -8,16 +8,14 @@ public class Flipping {
         int[][] res = flipAndInvertImage(image);
         System.out.print(Arrays.deepToString(res));
     }
-    public static int[][] flipAndInvertImage(int[][] image) {
-        int n = image.length;
-        int sam = (n + 1)/2;
-        for(int i = 0; i < n; i++) {
-            for (int j = 0; j < sam; j++) {
-                int temp = image[i][j] ^ 1;
-                image[i][j] = image[i][n - 1 - j] ^ 1;
-                image[i][n - 1 - j] = temp;
+        public static int[][] flipAndInvertImage(int[][] image) {
+            int n = image.length;
+            int[][] temp = new int[n][n];
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    temp[i][n - j - 1] = image[i][j] ^ 1;
+                }
             }
+            return temp;
         }
-        return image;
-    }
 }
