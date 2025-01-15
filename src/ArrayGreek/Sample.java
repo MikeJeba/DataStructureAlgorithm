88cd89202fde553a3fbc42758d9948122676f220
MikeJeba <jebajovitha@gmail.com>
2025-01-15 19:21:49 +0530

    SprialMatrixII

--- src/ArrayGreek/Sample.java
+++ src/ArrayGreek/Sample.java
@@ -4,47 +4,7 @@ import java.util.Arrays;
 
 public class Sample {
 
-    public static int[][] generateMatrix(int n) {
-        int[][] res = new int[n][n];
-        int num = 1;
-        int rowbegin = 0;
-        int colbegin = 0;
-        int rowend = n - 1;
-        int colend = n - 1;
-        while(rowbegin <= rowend && colbegin <= colend){
-            //transverse left
-            for(int i = colbegin; i <= colend; i++){
-                res[rowbegin][i] = num++;
-            }
-            rowbegin++;
-
-            //transverse down
-            for(int j = rowbegin; j <= rowend; j++){
-                res[j][colend] = num++;
-            }
-            colend--;
-
-            //transverse right
-            if(rowbegin <= rowend){
-                for(int k = colend; k >= colbegin; k--){
-                    res[rowend][k] = num++;
-                }
-            }
-            rowend--;
-
-            //transverse up
-            if(colbegin <= colend){
-                for(int l = rowend; l >= rowbegin; l--){
-                    res[l][colbegin] = num++;
-                }
-            }
-            colbegin++;
-        }
-        return res;
-    }
-
     public static void main(String[] args) {
-        int n = 3;
-        System.out.println(Arrays.deepToString(generateMatrix(n)));
+
     }
 }
--- src/ArrayMedium/SprialMatrixII.java
+++ src/ArrayMedium/SprialMatrixII.java
@@ -1,12 +1,43 @@
 package ArrayMedium;
 
+import java.util.Arrays;
+
 public class SprialMatrixII {
     public static void main(String[] args) {
-
+        int rows = 1;
+        int cols = 4;
+        int rStart = 0;
+        int cStart = 0;
+        System.out.println(Arrays.deepToString(spiralMatrixII(rows,cols,rStart,cStart)));
     }
-    public int spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        
-        return 0;
+    public static int[][] spiralMatrixII(int rows, int cols, int rStart, int cStart) {
+        int d = 0;
+        int count = 1;
+        int step = 0;
+        int[][] direction = {{0,1},{1,0},{0,-1},{-1,0}};
+        int[][] result = new int[rows * cols][];
+        result[0] = new int[]{rStart,cStart};
+
+        while(count < rows * cols){
+            if(d == 0 || d == 2){
+                step++;
+            }
+
+            for(int i = 0; i < step; i++){
+                rStart += direction[d][0];
+                cStart += direction[d][1];
+                if(rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols){
+                    result[count++] = new int[]{rStart,cStart};
+                }
+
+                if(count == rows * cols){
+                    return result;
+                }
+            }
+
+            d = (d + 1) % 4;
+        }
+        return result;
     }
 }
 
