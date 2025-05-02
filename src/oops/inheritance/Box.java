520ae8a9e5bad8a6861307fee3e894673ab35f07
MikeJeba <jebajovitha@gmail.com>
2025-05-02 22:14:53 +0530

    Inheritance

---
+++ src/oops/inheritance/Box.java
@@ -0,0 +1,12 @@
+package oops.inheritance;
+
+public class Box {
+    void BoxWeight(){
+        System.out.println("Box class");
+    }
+
+    public static void main(String[] args) {
+        Box obj = new BoxWeight();
+        obj.BoxWeight();
+    }
+}
---
+++ src/oops/inheritance/BoxWeight.java
@@ -0,0 +1,8 @@
+package oops.inheritance;
+
+public class BoxWeight extends Box{
+    @Override
+    void BoxWeight(){
+        System.out.println("Boxweight class");
+    }
+}
